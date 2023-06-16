a19d364215575a40674f7c8357d83caf9549244b
li-weihua <weihua.lee@qq.com>
2023-06-16 19:48:10 +0800

    [cmake] add Ch11_02

--- Chapter11/Ch11_01/Ch11_01_.s
+++ Chapter11/Ch11_01/Ch11_01_.s
@@ -6,21 +6,21 @@
 
 // extern "C" int IntegerAddSubA_(int a, int b int c);
 
-            .text
-            FUNCTION_ENTRY IntegerAddSubA_
+  .text
+  FUNCTION_ENTRY IntegerAddSubA_
 
 // Calculate a + b - c
-            add w3,w0,w1                            // w3 = a + b
-            sub w0,w3,w2                            // w0 = a + b - c
-            ret                                     // return to caller
+  add w8, w0, w1                            // w3 = a + b
+  sub w0, w8, w2                            // w0 = a + b - c
+  ret                                     // return to caller
 
 // extern "C" long IntegerAddSubB_(long a, long b long c);
 
-            FUNCTION_ENTRY IntegerAddSubB_
+  FUNCTION_ENTRY IntegerAddSubB_
 
 // Calculate a + b - c
-            add x3,x0,x1                            // x3 = a + b
-            sub x0,x3,x2                            // x0 = a + b - c
-            ret                                     // return to caller
+  add x8, x0, x1                            // x3 = a + b
+  sub x0, x8, x2                            // x0 = a + b - c
+  ret                                     // return to caller
 
-            .end
+  .end
